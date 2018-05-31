a612f2c63e511975ec957adddb8eac54cbd3e50c
aimeelally <aimeelally@hotmail.com>
2018-05-31 14:19:18 +0700

    removing gitignore files

--- src/views/landing-page/LandingPageController.js
+++ src/views/landing-page/LandingPageController.js
@@ -5,7 +5,55 @@ angular.module("landingPage", [])
     template: require("./landing-page.html"), 
     controller: [ '$scope',
       function LandingPageController($scope) {
-      
+        
+        $(document).ready(function(){
+
+        $('body').keydown(function(e) {
+          
+          // if(e.keyCode == 38) { // up key
+          //     $('.box')
+          //         .addClass('up')
+          //         .removeClass('left right down')
+          //         .stop()
+          //         .animate({
+          //             top: '-=400'
+          //         });
+          //   }
+          if(e.keyCode == 37) { // left key
+            $('.susie')
+                .addClass('left')
+                .removeClass('right down up')
+                .stop()
+                .animate({
+                   left: '-=100'
+               });
+          }
+          else if(e.keyCode == 39) { // right key
+            $('.susie')
+                .addClass('right')
+                .removeClass('left down up')
+                .stop()
+                .animate({
+                   left: '+=100'
+                });
+          }
+          // else if(e.keyCode == 40) { // down key
+          //   $('.box')
+          //     .addClass('down')
+          //     .removeClass('left right up')
+          //     .stop()
+          //     .animate({
+          //        top: '+=400'
+          //      });
+          // }
+        }).keyup(function(e){ 
+          // On keyup all direction classes are moved
+          $('.susie').removeClass('right down up left');
+        });
+      });
+
+
+
       }
         
     ]
