5ec3d8f2da2e2485a4ee417f95f80bd7f0028a64
aimeelally <aimeelally@hotmail.com>
2018-06-01 22:21:24 +0700

    allow continuous ovement

--- src/views/interactive-cv/InteractiveCvController.js
+++ src/views/interactive-cv/InteractiveCvController.js
@@ -5,141 +5,104 @@ angular.module("interactiveCv", [])
     template: require("./interactive-cv.html"), 
     controller: [ '$scope',
       function InteractiveCvController($scope) {
+
+        const FARTHEST_POINT_LEFT = 200;
+        const FARTHEST_POINT_RIGHT = -8000;
+
+        var movLeft = 0;
+        var movRight = 0;
+        var speedVSlow = 5,
+            speedSlow = 200,
+            speedMed = 300,
+            speedFast = 800,
+            speedVFast = 1000;
+
+        function animateRight(element, distance) {
+          $(element)
+            .addClass('right')
+            .removeClass('left down')
+            .stop()
+            .animate({
+              left: '+='+distance
+            });
+        }
+
+        function animateLeft(element, distance) {
+          $(element)
+            .addClass('left')
+            .removeClass('right down up')
+            .stop()
+            .animate({
+               left: '-='+distance
+           });
+        }
+
+        function isValidMove(direction) {
+          
+          var blockerPosition = $("#stop-scroll").offset().left;
+          var blockerPositionAfterMove = blockerPosition - speedMed;
+          
+          if(direction === 'left' && blockerPositionAfterMove <= FARTHEST_POINT_LEFT) {
+            return true;
+          }
+          else if(direction === 'right' && blockerPositionAfterMove >= FARTHEST_POINT_RIGHT) {
+            return true;
+          }
+
+          return;
+
+        }
         
         $(document).ready(function(){
             
-            /*var windowHeight = $(window).height();
-            $('.setPageHeight')
-                .css('height', windowHeight);*/
-
-            var keysdown = {};
-            var hasMadeFirstMove = false;
-            var speedVSlow = 5,
-                speedSlow = 200,
-                speedMed = 300,
-                speedFast = 800,
-                speedVFast = 1000;
-
-
-            /*// Bind the swipeHandler callback function to the swipe event on div.box
-            $( "div#interactive-cv" ).on( "swipe", swipeHandler );
-           
-            // Callback function references the event target and adds the 'swipe' class to it
-            function swipeHandler( event ){
-              $( event.target ).addClass( "swipe" );
-            }
-
-            $('body').scroll(function() {
-              debugger;
-                didScroll = true;
-            });*/
-
-            
-
-
-
-
-            //press button
-            //check if a move is valid before anything else
-            //if valid continue
-
-
-            function animateRight(element, distance) {
-              $(element)
-                .addClass('right')
-                .removeClass('left down')
-                .stop()
-                .animate({
-                  left: '+='+distance
-                });
-              // setTimeout(function(){
-              //   animateRight(element,distance);
-              // },500);
-            }
-
-            function animateLeft(element, distance) {
-              $(element)
-                .addClass('left')
-                .removeClass('right down up')
-                .stop()
-                .animate({
-                   left: '-='+distance
-               });
-            }
-
-            function isValidMove(direction) {
-
-              const FARTHEST_POINT_LEFT = 200;
-              const FARTHEST_POINT_RIGHT = 1000;
-              
-              var blockerPosition = $("#stop-scroll").offset().left;
-              var blockerPositionAfterMove = blockerPosition - speedMed;
-
-              
-              if(direction === 'left' && blockerPositionAfterMove <= 200) {
-                return true;
-              }
-              else if(direction === 'right' && blockerPositionAfterMove >= -8000) {
-                return true;
-              }
-
-              return;
-
-            }
-
-            /*var lastScrollTop = 0;
-            $('div#interactive-cv').scroll(function(){
-              debugger;
-               var st = $(this).scrollTop();
-               if (st > lastScrollTop){
-                   // downscroll code
-                   $('.susie').removeClass('face-left');
-                    $('.susie').addClass('face-right');
-                    animateRight('.susie', 0);
-                    
-                    animateLeft('.med-moving-items', speedMed);
-                    animateLeft('.slow-moving-items', speedSlow);
-                    animateLeft('.v-slow-moving-items', speedVSlow);
-               } else {
-                  // upscroll code
-                  $('.susie').removeClass('face-right');
-                  $('.susie').addClass('face-left');
-                  animateLeft('.susie', 0);
-
-                  animateRight('.med-moving-items', speedMed);
-                  animateRight('.slow-moving-items', speedSlow);
-                  animateRight('.v-slow-moving-items', speedVSlow);
-               }
-               lastScrollTop = st;
-            });*/
-
             $('body').keydown(function(e) {
 
-              //console.log($('.ground-container').offset().left);
-              if (keysdown[e.keyCode]) {
-                // Ignore it
-                return;
-              }
+              var ek = e.keyCode;
+              if (ek==39) movRight=1;
+              if (ek==38) movRight=1;
+              if (ek==37) movLeft=1;
+              if (ek==40) movLeft=1;
               
-              // Remember it's down
-              keysdown[e.keyCode] = true;
-              
-              if(e.keyCode == 38 || e.keyCode == 39) { // up or right key
-                
+              setInterval(movTick(), 1000); // Setup interval. Delay controlls tickrate.
+
+            });
+
+            // Keyup listener
+            $("body").keyup(function(e) {
+              var ek = e.keyCode;
+              if (ek==39) movRight=0;
+              if (ek==38) movRight=0;
+              if (ek==37) movLeft=0;
+              if (ek==40) movLeft=0;
+
+              $('.susie').removeClass('right left');
+
+            });
+
+            // Move one pixel for each direction and check if move is valid.
+            function movTick() {
+              console.log('tick');
+
+              var moved = 0;
+              if (movRight) { 
+
                 if(!isValidMove('right')) {
                   return;
                 }
 
                 $('.susie').removeClass('face-left');
                 $('.susie').addClass('face-right');
+                
                 animateRight('.susie', 0);
                 
                 animateLeft('.med-moving-items', speedMed);
                 animateLeft('.slow-moving-items', speedSlow);
                 animateLeft('.v-slow-moving-items', speedVSlow);
-
+                
+                moved = 1;
               }
-              else if(e.keyCode == 37 || e.keyCode == 40) { // left or down key
+
+              if (movLeft) { 
                 if(!isValidMove('left')) {
                   return;
                 }
@@ -151,16 +114,16 @@ angular.module("interactiveCv", [])
                 animateRight('.med-moving-items', speedMed);
                 animateRight('.slow-moving-items', speedSlow);
                 animateRight('.v-slow-moving-items', speedVSlow);
-              }
 
-            }).keyup(function(e){ 
-              // On keyup all direction classes are moved
-              delete keysdown[e.keyCode];
-              setTimeout(function() {
-                $('.susie').removeClass('right left');
-              }, 500);
+                moved = 1;
+              }
               
-            });
+
+              if (!moved) return false;
+                
+            }
+
+
           });
 
 
